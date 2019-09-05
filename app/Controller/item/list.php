9c68b1b5ca87f61753d60fce070e028cdac30671
shintaro kondo <nekomo@outlook.jp>
2019-09-05 15:45:19 +0900

    created ItemList
    リスト形式でアイテムを一覧表示するページを作成しました。
    それに関連して、DataBaseのmovieテーブルから全件検索する新規メソッドをMovie.phpに作成した。

--- app/Controller/item/list.php
+++ app/Controller/item/list.php
@@ -1,6 +1,6 @@
 <?php
 
-use Model\Dao\Item;
+use Model\Dao\Movie;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -10,10 +10,10 @@ $app->get('/item/list/', function (Request $request, Response $response) {
     $data=[];
 
     //アイテムDAOをインスタンス化します。
-    $item = new Item($this->db);
+    $movie = new Movie($this->db);
 
     //アイテム一覧を取得し、戻り値をresultに格納します
-    $data["result"] = $item->getItemList();
+    $data["result"] = $movie->getItemList();
 
     // Render index view
     return $this->view->render($response, 'item/list.twig', $data);
--- app/Model/Dao/movie.php
+++ app/Model/Dao/movie.php
@@ -16,5 +16,30 @@ namespace Model\Dao;
 class Movie extends Dao
 {
 
+    /**
+     * getItemList Function
+     *
+     * Itemテーブルにあるアイテム一覧を取得するためのサンプルです。
+     *
+     * @return array $result 結果情報を連想配列で指定します。
+     * @throws DBALException
+     * @since 2019/08/14
+     * @copyright Ceres inc.
+     * @author y-fukumoto <y-fukumoto@ceres-inc.jp>
+     */
+    public function getItemList()
+    {
+        //全件取得するクエリを作成
+        $sql = "select * from movie";
+
+        // SQLをプリペア
+        $statement = $this->db->prepare($sql);
+
+        //SQLを実行
+        $statement->execute();
+
+        //結果レコードを全件取得し、返送
+        return $statement->fetchAll();
+    }
 
 }
