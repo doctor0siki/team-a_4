6694eac0d2b144a40e84afc3ce26dfa53c31bb1c
shintaro kondo <nekomo@outlook.jp>
2019-09-06 13:59:17 +0900

    Modified User
    ユーザー画面ごとに自身の動画が出せるようにしました〜

--- app/Controller/user/index.php
+++ app/Controller/user/index.php
@@ -3,20 +3,28 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Model\Dao\User;
+use Model\Dao\Movie;
 
 // 動画確認画面コントローラ
 $app->get('/user/', function (Request $request, Response $response) {
 
     // $dd(user_info); 正常に取れています。
     //GETされた内容を取得します。
-    $data = $this->session["user_info"];
+    $session_user = $this->session["user_info"];
 
-    if (!$data ) {
+    if (!$session_user ) {
       return $response->withRedirect('/login/');
     }
 
     // user idを持ってくる。
+    //アイテムDAOをインスタンス化します。
+    $movie = new Movie($this->db);
+    $data=[];
+    $data["name"]=$session_user["name"];
+    $data["email"]=$session_user["email"];
 
-
+    //アイテム一覧を取得し、戻り値をresultに格納します
+    $data["result"] = $movie->getItemListOfUser($session_user['id']);
+    #dd($data["result"]);
     return $this->view->render($response, '/user/user.twig', $data);
 });
